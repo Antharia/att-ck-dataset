a3179831bed4cb7aa8c3c60e1010b2f99643c03e
Antharia <artenischloria@gmail.com>
2021-12-07 15:20:10 +0100

    FEAT list aliases

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 import mad
-import stix2
+
+def prettify(output):
+    return output.serialize(pretty=True)
+
+def get_aliases(src, alias):
+    """
+    Get all aliases of one group by name
+    """
+    aliases = mad.get_group_by_alias(src, alias).aliases
+    print(f"Aliases of {alias} :")
+    for a in aliases:
+        print(f" - {a}")
+    return mad.get_group_by_alias(src, alias).aliases
 
 src = mad.FileSystemSource("./cti/enterprise-attack")
 
+get_aliases(src, "APT29")
+
 output = mad.get_group_by_alias(src, "Cozy Bear")
-output = output.serialize(pretty=True)
-print(str(output))
+print(prettify(output))
+
+
