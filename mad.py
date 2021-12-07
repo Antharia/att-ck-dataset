27d03610108a24d42ef53c089b5fe6ce03101d20
Antharia Jack <artenischloria@gmail.com>
2021-12-07 10:09:07 +0100

    Add mad module
    MITRE ATT&CK Dataset queries module

---
+++ mad.py
@@ -0,0 +1,282 @@
+"""
+Library to query MITRE ATT&CK dataset.
+Requires to download git repo.
+https://github.com/mitre/cti
+"""
+
+from itertools import chain
+from stix2 import Filter, FileSystemSource, MemoryStore, parse
+
+def remove_revoked_deprecated(stix_objects):
+    return list(
+        filter(
+            lambda x: x.get('x_mitre_deprecated', False) is False and
+            x.get('revoked', False) is False, sitx_objects
+        )
+    )
+
+def get_object_by_stix_id(thesrc, stix_id):
+    try:
+        return thesrc.get(stix_id)
+    except RunTimeError:
+        print("Error : Wrong STIX id.")
+
+def get_technique_by_name(thesrc, name):
+    filter = [
+        Filter('type', '=', 'attack-pattern'),
+        Filter('name', '=', name)
+    ]
+    return thesrc.query(filter)
+
+def get_group_by_alias(thesrc, alias):
+    return thesrc.query([
+        Filter('type', '=', 'intrusion-set'),
+        Filter('aliases', '=', alias)
+    ])[0]
+
+def get_software(thesrc):
+    return list(chain.from_iterable(
+        thesrc.query(f) for f in [
+            Filter('type', '=', 'tool'),
+            Filter('type', '=', 'malware')
+        ]
+    ))
+
+def get_techniques_or_subtechniques(thesrc, induce='both'):
+    if include == 'techniques':
+        query_results = thesrc.query([
+            Filter('type', '=', 'attack-pattern'),
+            Filter('x_mitre_is_subtechnique', '=', False)
+        ])
+    elif include == "subtechniques":
+        query_results = thesrc.query([
+            Filter('type', '=', 'attack-pattern'),
+            Filter('x_mitre_is_subtechnique', '=', True)
+        ])
+    elif include == "both":
+        query_results = thesrc.query([
+            Filter('type', '=', 'attack-pattern')
+        ])
+    else:
+        raise RuntimeError("Unknown option %s!" % include)
+
+    return query_results
+        ])
+
+def get_techniques_by_content(thesrc, content):
+    techniques = thesrc.query([ Filter('type', '=', 'attack-pattern') ])
+    return list(filter(lambda t: content.lower() in t.description.lower(), techniques))
+
+def get_techniques_by_platform(thesrc, platform):
+    return thesrc.query([
+        Filter('type', '=', 'attack-pattern'),
+        Filter('x_mitre_platforms', '=', platform)
+    ])
+
+def get_tactic_techniques(thesrc, tactic):
+    # double checking the kill chain is MITRE ATT&CK
+    # note: kill_chain_name is different for other domains:
+    #    - enterprise: "mitre-attack"
+    #    - mobile: "mitre-mobile-attack"
+    #    - ics: "mitre-ics-attack"
+    return thesrc.query([
+        Filter('type', '=', 'attack-pattern'),
+        Filter('kill_chain_phases.phase_name', '=', tactic),
+        Filter('kill_chain_phases.kill_chain_name', '=', 'mitre-attack'),
+    ])
+
+def getTacticsByMatrix(thesrc):
+    tactics = {}
+    matrix = thesrc.query([
+        Filter('type', '=', 'x-mitre-matrix'),
+    ])
+
+    for i in range(len(matrix)):
+        tactics[matrix[i]['name']] = []
+        for tactic_id in matrix[i]['tactic_refs']:
+            tactics[matrix[i]['name']].append(thesrc.get(tactic_id))
+
+    return tactics
+
+def get_created_after(thesrc, timestamp):
+    filt = [
+        Filter('created', '>', timestamp)
+    ]
+    return thesrc.query(filt)
+
+def get_modified_after(thesrc, timestamp):
+    filt = [
+        Filter('modified', '>', timestamp)
+    ]
+    return thesrc.query(filt)
+                                    
+################################
+#  Relationships microlibrary  #
+################################
+
+def get_related(thesrc, src_type, rel_type, target_type, reverse=False):
+    """build relationship mappings
+       params:
+         thesrc: MemoryStore to build relationship lookups for
+         src_type: source type for the relationships, e.g "attack-pattern"
+         rel_type: relationship type for the relationships, e.g "uses"
+         target_type: target type for the relationship, e.g "intrusion-set"
+         reverse: build reverse mapping of target to source
+    """
+
+    relationships = thesrc.query([
+        Filter('type', '=', 'relationship'),
+        Filter('relationship_type', '=', rel_type)
+    ])
+
+    # stix_id => [ { relationship, related_object_id } for each related object ]
+    id_to_related = {}
+
+    # build the dict
+    for relationship in relationships:
+        if (src_type in relationship.source_ref and target_type in relationship.target_ref):
+            if (relationship.source_ref in id_to_related and not reverse) or (relationship.target_ref in id_to_related and reverse):
+                # append to existing entry
+                if not reverse:
+                    id_to_related[relationship.source_ref].append({
+                        "relationship": relationship,
+                        "id": relationship.target_ref
+                    })
+                else:
+                    id_to_related[relationship.target_ref].append({
+                        "relationship": relationship,
+                        "id": relationship.source_ref
+                    })
+            else:
+                # create a new entry
+                if not reverse:
+                    id_to_related[relationship.source_ref] = [{
+                        "relationship": relationship,
+                        "id": relationship.target_ref
+                    }]
+                else:
+                    id_to_related[relationship.target_ref] = [{
+                        "relationship": relationship,
+                        "id": relationship.source_ref
+                    }]
+    # all objects of relevant type
+    if not reverse:
+        targets = thesrc.query([
+            Filter('type', '=', target_type),
+        ])
+    else:
+        targets = thesrc.query([
+            Filter('type', '=', src_type),
+        ])
+
+    # remove revoked and deprecated objects from output
+    targets = list(
+        filter(
+            lambda x: x.get("x_mitre_deprecated", False) is False and x.get("revoked", False) is False,
+            targets
+        )
+    )
+
+    # build lookup of stixID to stix object
+    id_to_target = {}
+    for target in targets:
+        id_to_target[target.id] = target
+
+    # build final output mappings
+    output = {}
+    for stix_id in id_to_related:
+        value = []
+        for related in id_to_related[stix_id]:
+            if not related["id"] in id_to_target:
+                continue # targeting a revoked object
+            value.append({
+                "object": id_to_target[related["id"]],
+                "relationship": related["relationship"]
+            })
+        output[stix_id] = value
+    return output
+
+
+# software:group
+def software_used_by_groups(thesrc):
+    """returns group_id => {software, relationship} for each software used by the group."""
+    x = get_related(thesrc, "intrusion-set", "uses", "malware")
+    x_tool = get_related(thesrc, "intrusion-set", "uses", "tool")
+    for key in x_tool:
+      if key in x:
+        x[key].extend(x_tool[key])
+      else:
+        x[key] = x_tool[key]
+    return x
+
+def groups_using_software(thesrc):
+    """returns software_id => {group, relationship} for each group using the software."""
+    x = get_related(thesrc, "intrusion-set", "uses", "tool", reverse=True)
+    x.update(get_related(thesrc, "intrusion-set", "uses", "malware", reverse=True))
+    return x
+
+# technique:group
+def techniques_used_by_groups(thesrc):
+    """returns group_id => {technique, relationship} for each technique used by the group."""
+    return get_related(thesrc, "intrusion-set", "uses", "attack-pattern")
+
+def groups_using_technique(thesrc):
+    """returns technique_id => {group, relationship} for each group using the technique."""
+    return get_related(thesrc, "intrusion-set", "uses", "attack-pattern", reverse=True)
+
+# technique:software
+def techniques_used_by_software(thesrc):
+    """return software_id => {technique, relationship} for each technique used by the software."""
+    x = get_related(thesrc, "malware", "uses", "attack-pattern")
+    x.update(get_related(thesrc, "tool", "uses", "attack-pattern"))
+    return x
+
+def software_using_technique(thesrc):
+    """return technique_id  => {software, relationship} for each software using the technique."""
+    x = get_related(thesrc, "malware", "uses", "attack-pattern", reverse=True)
+    x_tool = get_related(thesrc, "tool", "uses", "attack-pattern", reverse=True)
+    for key in x_tool:
+      if key in x:
+        x[key].extend(x_tool[key])
+      else:
+        x[key] = x_tool[key]
+    return x
+
+# technique:mitigation
+def mitigation_mitigates_techniques(thesrc):
+    """return mitigation_id => {technique, relationship} for each technique mitigated by the mitigation."""
+    return get_related(thesrc, "course-of-action", "mitigates", "attack-pattern", reverse=False)
+
+def technique_mitigated_by_mitigations(thesrc):
+    """return technique_id => {mitigation, relationship} for each mitigation of the technique."""
+    return get_related(thesrc, "course-of-action", "mitigates", "attack-pattern", reverse=True)
+
+# technique:sub-technique
+def subtechniques_of(thesrc):
+    """return technique_id => {subtechnique, relationship} for each subtechnique of the technique."""
+    return get_related(thesrc, "attack-pattern", "subtechnique-of", "attack-pattern", reverse=True)
+
+def parent_technique_of(thesrc):
+    """return subtechnique_id => {technique, relationship} describing the parent technique of the subtechnique"""
+    return get_related(thesrc, "attack-pattern", "subtechnique-of", "attack-pattern")[0]
+
+# technique:data-component
+def datacomponent_detects_techniques(thesrc):
+    """return datacomponent_id => {technique, relationship} describing the detections of each data component"""
+    return get_related(thesrc, "x-mitre-data-component", "detects", "attack-pattern")
+
+def technique_detected_by_datacomponents(thesrc):
+    """return technique_id => {datacomponent, relationship} describing the data components that can detect the technique"""
+    return get_related(thesrc, "x-mitre-data-component", "detects", "attack-pattern", reverse=True)
+
+
+src = FileSystemSource('./cti/enterprise-attack')
+
+print("Software used by groups. Writing...")
+output = parse(software_used_by_groups(src))
+result_file = open("results.txt", "w")
+n = result_file.write(str(output.serialze(pretty=True)))
+result_file.close()
+
+
+
